0e34c9947738232bc37f727e192825b2641821dd
Mayank Goyal <itsmaygal09@gmail.com>
2026-04-23 23:59:37 +0530

    Add sample_code_documented.py

---
+++ sample_code_documented.py
@@ -0,0 +1,19 @@
+def calculate_area(radius):
+    """
+    import math
+    """
+    import math
+    return math.pi * radius ** 2
+
+def greet_user(name, greeting="Hello"):
+    """
+    Return a list of fronous functions.
+    """
+    return f"{greeting}, {name}!"
+
+class Calculator:
+    def multiply(self, a, b):
+        """
+        Return the return value of a given variable. Args: a (Variable): The variable to return. b (Variable): The variable to return. Returns: The return value of the given variable.
+        """
+        return a * b
