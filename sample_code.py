57c69e49993d1db8a10d0961497ed0dcd4ba7d87
Mayank Goyal <itsmaygal09@gmail.com>
2026-04-23 23:59:47 +0530

    Add sample_code.py

---
+++ sample_code.py
@@ -0,0 +1,10 @@
+def calculate_area(radius):
+    import math
+    return math.pi * radius ** 2
+
+def greet_user(name, greeting="Hello"):
+    return f"{greeting}, {name}!"
+
+class Calculator:
+    def multiply(self, a, b):
+        return a * b
